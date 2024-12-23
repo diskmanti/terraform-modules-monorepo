513a25f2a4f1361c3c3d91c8a8ff2b47be25de92
Amanti Lulo <amanti.lulo@lhind.dlh.de>
2024-12-23 16:06:47 +0100

    removed unused providers

--- kind-cluster/terraform.tf
+++ kind-cluster/terraform.tf
@@ -1,24 +1,10 @@
 terraform {
   required_version = "~> 1.7"
   required_providers {
-
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.18.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.35.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.16.1"
-    }
     kind = {
       source  = "tehcyx/kind"
       version = ">= 0.6.0"
     }
-
     null = {
       source  = "hashicorp/null"
       version = ">= 3.2.3"
